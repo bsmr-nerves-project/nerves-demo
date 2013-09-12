8287048bb4ac1161262e2c9242b48349ba5d3073
Frank Hunleth <fhunleth@troodon-software.com>
2013-09-12 15:37:42 -0400

    Add note about state; keep debug in until it is fixed

--- apps/demo/src/demo_app.erl
+++ apps/demo/src/demo_app.erl
@@ -16,6 +16,7 @@ start(_StartType, _StartArgs) ->
 
     % Bring up the ethernet interface to a hardcoded IP
     % address for now.
+    os:cmd("/sbin/ip link set lo up"),
     os:cmd("/sbin/ip link set eth0 up"),
     os:cmd("/sbin/ip addr add 192.168.1.80/24 dev eth0"),
     os:cmd("/sbin/ip route add default via 192.168.1.1"),
--- apps/demo/src/site_index.erl
+++ apps/demo/src/site_index.erl
@@ -2,6 +2,9 @@
 
 -export([init/3, handle/2, terminate/3]).
 
+% This state doesn't work the way that I originally thought.
+% Real state should be kept somewhere else, since Cowboy doesn't
+% keep updates to state that are returned from handle/2.
 -record(state, {onoff=false}).
 
 init(_TransportAndProtocol, Req, _Options) -> 
@@ -14,6 +17,7 @@ boolean_from_form(Key, Props) ->
     end.
     
 handle(Req, State) ->
+    io:format("State=~p~n", [State]),
     case cowboy_req:method(Req) of
 	{<<"GET">>,_Req} ->
 	    {ok, Req2} = serve_page(index_tpl, [{onoff,State#state.onoff}], Req),
@@ -21,9 +25,9 @@ handle(Req, State) ->
 	{<<"POST">>,_Req} ->
 	    {ok, Props, _Req2} = cowboy_req:body_qs(Req),
 	    NewOnoff = boolean_from_form(<<"onoff">>, Props),
-	    io:format("NewOnoff=~p~n", [NewOnoff]),
 	    State2 = State#state{onoff=NewOnoff},
 	    {ok, Req2} = serve_page(index_tpl, [{onoff,NewOnoff}], Req),
+	    io:format("State2=~p~n", [State2]),
 	    {ok, Req2, State2}
     end.
 	    
